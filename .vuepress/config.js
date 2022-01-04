f0fdeca4d6e778a20d89d6e092225a251457e1da
kinghao <362253572@qq.com>
2022-01-04 17:33:51 +0800

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+// THIS IS FILE IS OPTIONAL, you can delete it if you don't want to use it
+
+// config.js is the entry file for your VuePress app configuration
+// It can also be written in yml or toml instead of js
+// See the documentation for more information on how to use it
+// https://v1.vuepress.vuejs.org/config/
+
+module.exports = {
+  title: "EASY-ES6",
+  description: "EASY-ES6 with VuePress",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "ES6", link: "/es6/" },
+      { text: "GitHub", link: "https://github.com/kingfront" }
+    ]
+  }
+};
